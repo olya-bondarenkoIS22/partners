290f74e9da280ffe59ebd7ab63c6ed63758fa68a
bondarenkoola228@gmail.com <bondarenkoola228@gmail.com>
2025-03-11 11:44:15 +0500

    часть кода

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,43 +29,87 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            button1 = new Button();
+            buttonSave = new Button();
+            panel2 = new Panel();
+            label1 = new Label();
+            dataGridViewPartners = new DataGridView();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPartners).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Controls.Add(button1);
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(buttonSave);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1214, 87);
+            panel1.Size = new Size(631, 62);
             panel1.TabIndex = 0;
             // 
-            // button1
+            // buttonSave
             // 
-            button1.Location = new Point(12, 21);
-            button1.Name = "button1";
-            button1.Size = new Size(208, 39);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            buttonSave.BackColor = Color.WhiteSmoke;
+            buttonSave.Location = new Point(12, 12);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(208, 39);
+            buttonSave.TabIndex = 0;
+            buttonSave.Text = "button1";
+            buttonSave.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(dataGridViewPartners);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 62);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(631, 293);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Партнеры";
+            // 
+            // dataGridViewPartners
+            // 
+            dataGridViewPartners.BackgroundColor = Color.Gainsboro;
+            dataGridViewPartners.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPartners.Location = new Point(12, 27);
+            dataGridViewPartners.Name = "dataGridViewPartners";
+            dataGridViewPartners.Size = new Size(607, 241);
+            dataGridViewPartners.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1214, 693);
+            ClientSize = new Size(631, 355);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPartners).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
-        private Button button1;
+        private Button buttonSave;
+        private Panel panel2;
+        private DataGridView dataGridViewPartners;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,28 @@
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
 namespace partners
 {
     public partial class Form1 : Form
     {
+        private Pr4Context? db;
         public Form1()
         {
             InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.db = new Pr4Context();
+            this.db.Partners.Load();
+            this.dataGridViewPartners.DataSource = db.Partners.Local.ToBindingList();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.db?.Dispose();
+            this.db = null;
         }
     }
 }
